e469aacd1d69274af061c3244818358b589cfa11
Jingwei <jingweim@mit.edu>
2019-09-15 05:57:15 -0400

    bbox

--- posenet-python/gapfill.py
+++ posenet-python/gapfill.py
@@ -6,6 +6,7 @@ import os
 
 import posenet
 import json
+import numpy as np
 
 MEMORY = 2
 t_coeffs = [0.5**(MEMORY - m - (m == 0)) for m in range(MEMORY)]
@@ -42,7 +43,57 @@ def apply_smoothing(prev_scores, prev_coords, curr_scores, curr_coords):
         out_coords.append([out_x, out_y])
     return out_scores, out_coords
 
+def return_max_area_id(keypoint_coords, pose_scores):
+    area = -1
+    idx = None
+    bbox = None
+    for pi in range(len(pose_scores)):
+        if pose_scores[pi] == 0.:
+            break
+        pose = keypoint_coords[pi, :, :]
+        xmin = np.min(pose[:, 0])
+        xmax = np.max(pose[:, 0])
+        ymin = np.min(pose[:, 1])
+        ymax = np.max(pose[:, 1])
+        new_area = (xmax-xmin) * (ymax-ymin)
+        if new_area > area:
+            area = new_area
+            idx = pi
+            bbox = [int(xmin), int(xmax), int(ymin), int(ymax)]
+    return idx, bbox
 
+def create_bbox(keypoint_coords, pose_scores):
+    bbox = []
+    for pi in range(len(pose_scores)):
+        if pose_scores[pi] == 0.:
+            break
+        pose = keypoint_coords[pi, :, :]
+        xmin = np.min(pose[:, 0])
+        xmax = np.max(pose[:, 0])
+        ymin = np.min(pose[:, 1])
+        ymax = np.max(pose[:, 1])
+        bbox.append([int(xmin), int(xmax), int(ymin), int(ymax)])
+    return bbox
+
+def get_iou(bb1, bb2):
+    #print(bb1, bb2)
+    assert bb1[2] < bb1[3]
+    assert bb1[0] < bb1[1]
+    assert bb2[2] < bb2[3]
+    assert bb2[0] < bb2[1]
+    x_left = max(bb1[2], bb2[2])
+    y_top = max(bb1[0], bb2[0])
+    x_right = min(bb1[3], bb2[3])
+    y_bottom = min(bb1[1], bb2[1])
+    if x_right < x_left or y_bottom < y_top:
+        return 0.0
+    intersection_area = (x_right - x_left) * (y_bottom - y_top)
+    bb1_area = (bb1[3] - bb1[2]) * (bb1[1] - bb1[0])
+    bb2_area = (bb2[3] - bb2[2]) * (bb2[1] - bb2[0])
+    iou = intersection_area / float(bb1_area + bb2_area - intersection_area)
+    assert iou >= 0.0
+    assert iou <= 1.0
+    return iou
 
 def main():
 
@@ -65,7 +116,9 @@ def main():
             f.path for f in os.scandir(args.image_dir) if f.is_file() and f.path.endswith(('.png', '.jpg'))])
 
         start = time.time()
-        for f in filenames:
+        previous_id = None
+        previous_bbox = None
+        for idx, f in enumerate(filenames):
             input_image, draw_image, output_scale = posenet.read_imgfile(
                 f, scale_factor=args.scale_factor, output_stride=output_stride)
 
@@ -84,23 +137,38 @@ def main():
                 min_pose_score=0.25)
 
             keypoint_coords *= output_scale
-
+            bboxes = create_bbox(keypoint_coords, pose_scores)
+            if previous_id:
+                iou = -1
+                for key, item in enumerate(bboxes):
+                    new_iou = get_iou(item, previous_bbox)
+                    iou = max(iou, new_iou)
+                    if iou == new_iou:
+                        current_id = key
+                        current_bbox = item
+            else:
+                current_id,  current_bbox = return_max_area_id(keypoint_coords, pose_scores)
+            
             # smoothing function
-            past_coords.append(keypoint_coords[0])
+            past_coords.append(keypoint_coords[current_id])
             past_coords = past_coords[1:]
-            past_scores.append(keypoint_scores[0])
+            past_scores.append(keypoint_scores[current_id])
             past_scores = past_scores[1:]
 
-            smoothed_scores, smoothed_coords = apply_smoothing(past_scores, past_coords, keypoint_scores[0], keypoint_coords[0])
-            keypoint_scores[0] = smoothed_scores
-            keypoint_coords[0] = smoothed_coords
+            smoothed_scores, smoothed_coords = apply_smoothing(past_scores, past_coords, keypoint_scores[current_id], keypoint_coords[current_id])
+            keypoint_scores[current_id] = smoothed_scores
+            keypoint_coords[current_id] = smoothed_coords
 
+            previous_id = current_id
+            previous_bbox = current_bbox
             if args.output_dir:
                 draw_image = posenet.draw_skel_and_kp(
                     draw_image, pose_scores, keypoint_scores, keypoint_coords,
                     min_pose_score=0.25, min_part_score=0)
-#                cv2.imshow("out", draw_image)
-#                cv2.waitKey(1)
+                #_ = cv2.rectangle(draw_image, (previous_bbox[2], previous_bbox[0]), (previous_bbox[3], previous_bbox[1]), (0, 0, 255), 2)
+                #_ = cv2.rectangle(draw_image, (current_bbox[2], current_bbox[0]), (current_bbox[3], current_bbox[1]), (255, 0, 0), 2)
+                #cv2.imshow("out", draw_image)
+                #cv2.waitKey(1)
                 cv2.imwrite(os.path.join(args.output_dir, os.path.relpath(f, args.image_dir)), draw_image)
 
             data = dict()
