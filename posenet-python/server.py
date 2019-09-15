446d35da8941c1e90b72bf6891703679dfbd18cb
Jingwei <jingweim@mit.edu>
2019-09-15 08:32:20 -0400

    server thing first

--- posenet-python/server.py
+++ posenet-python/server.py
@@ -11,6 +11,10 @@ import tensorflow as tf
 import cv2
 import argparse
 import posenet
+import base64
+import numpy as np
+from PIL import Image
+import io
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--model', type=int, default=101)
@@ -34,11 +38,16 @@ def client_left(client, server):
 # Called when a client sends a message
 def message_received(client, server, message):
         print("Client(%d) said: %s" % (client['id'], message))
-        png_recovered = base64.decodebytes(message)
-        img = np.frombuffer(png_recovered, dtype=np.float64)
+        png_recovered = base64.decodebytes(base64.b64decode(message))
+        imgdata = base64.b64decode(message)
+        image = Image.open(io.BytesIO(imgdata))
+
+        img = cv2.cvtColor(np.array(image), cv2.COLOR_BGR2RGB)
+        #img = np.frombuffer(png_recovered, dtype="uint8")
 
         # img is what you decode, a numpy array
-        input_image, draw_image, output_scale = posenet._process_input(img, scale_factor, output_stride)
+        scale_factor=0.7125
+        input_image, draw_image, output_scale = posenet.process_input(img, scale_factor, output_stride)
         heatmaps_result, offsets_result, displacement_fwd_result, displacement_bwd_result = sess.run(model_outputs,feed_dict={'image:0': input_image})
         pose_scores, keypoint_scores, keypoint_coords = posenet.decode_multiple_poses(
                 heatmaps_result.squeeze(axis=0),
@@ -51,9 +60,9 @@ def message_received(client, server, message):
         keypoint_coords *= output_scale
         result = keypoint_coords
         for pi in range(len(pose_scores)):
-                if pose_scores[pi] == 0.:
+            if pose_scores[pi] == 0.:
                 result = keypoint_coords[:pi]
-        
+        print(result[0])        
         server.send_message_to_all(result)
 
 PORT=5000
