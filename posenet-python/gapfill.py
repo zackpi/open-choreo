c4c3800bc66d80331a859073d9307424324d2ae8
Zachary Pitcher <zackpi@mit.edu>
2019-09-15 01:48:34 -0400

    smoothing memory of 1

---
+++ posenet-python/gapfill.py
@@ -0,0 +1,124 @@
+import tensorflow as tf
+import cv2
+import time
+import argparse
+import os
+
+import posenet
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=int, default=101)
+parser.add_argument('--scale_factor', type=float, default=1.0)
+parser.add_argument('--notxt', action='store_true')
+parser.add_argument('--image_dir', type=str, default='./images')
+parser.add_argument('--output_dir', type=str, default='./output')
+args = parser.parse_args()
+
+
+# def apply_smoothing(prev_scores, prev_coords, curr_scores, curr_coords):
+#     out_scores = []
+#     out_coords = []
+
+#     for i in range(17):     # num key points
+#         prev_score = prev_scores[i]
+#         prev_coord = prev_coords[i]
+#         curr_score = curr_scores[i]
+#         curr_coord = curr_coords[i]
+
+#         inv_score_sum = 1 / (prev_score + curr_score)
+#         out_score = prev_score * 0.25 + curr_score * 0.75
+#         out_coord = [prev_coord[0] + prev_coord[0] * prev_score * inv_score_sum + curr_coord[0] * curr_score * inv_score_sum,
+#             prev_coord[1] + prev_coord[1] * prev_score * inv_score_sum + curr_coord[1] * curr_score * inv_score_sum]
+
+#         out_scores.append(out_score)
+#         out_coords.append(out_coord)
+#     return out_scores, out_coords
+
+
+def apply_smoothing(prev_scores, prev_coords, curr_scores, curr_coords):
+    out_scores = []
+    out_coords = []
+
+    for i in range(17):     # num key points
+        prev_score = prev_scores[i]
+        prev_coord = prev_coords[i]
+        curr_score = curr_scores[i]
+        curr_coord = curr_coords[i]
+
+        inv_score_sum = 1 / (prev_score + curr_score)
+        out_score = prev_score * 0.25 + curr_score * 0.75
+        out_coord = [prev_coord[0] + prev_coord[0] * prev_score * inv_score_sum + curr_coord[0] * curr_score * inv_score_sum,
+            prev_coord[1] + prev_coord[1] * prev_score * inv_score_sum + curr_coord[1] * curr_score * inv_score_sum]
+
+        out_scores.append(out_score)
+        out_coords.append(out_coord)
+    return out_scores, out_coords
+
+
+
+def main():
+
+    past_coords = [[0, 0] for _ in range(17)]
+    past_scores = [0] * 17
+
+    with tf.Session() as sess:
+        model_cfg, model_outputs = posenet.load_model(args.model, sess)
+        output_stride = model_cfg['output_stride']
+
+        if args.output_dir:
+            if not os.path.exists(args.output_dir):
+                os.makedirs(args.output_dir)
+
+        filenames = sorted([
+            f.path for f in os.scandir(args.image_dir) if f.is_file() and f.path.endswith(('.png', '.jpg'))])
+
+        start = time.time()
+        for f in filenames:
+            input_image, draw_image, output_scale = posenet.read_imgfile(
+                f, scale_factor=args.scale_factor, output_stride=output_stride)
+
+            heatmaps_result, offsets_result, displacement_fwd_result, displacement_bwd_result = sess.run(
+                model_outputs,
+                feed_dict={'image:0': input_image}
+            )
+
+            pose_scores, keypoint_scores, keypoint_coords = posenet.decode_multiple_poses(
+                heatmaps_result.squeeze(axis=0),
+                offsets_result.squeeze(axis=0),
+                displacement_fwd_result.squeeze(axis=0),
+                displacement_bwd_result.squeeze(axis=0),
+                output_stride=output_stride,
+                max_pose_detections=10,
+                min_pose_score=0.25)
+
+            keypoint_coords *= output_scale
+
+            # smoothing function
+            smoothed_scores, smoothed_coords = apply_smoothing(past_scores, past_coords, keypoint_scores[0], keypoint_coords[0])
+            keypoint_scores[0] = smoothed_scores
+            keypoint_coords[0] = smoothed_coords
+
+            if args.output_dir:
+                draw_image = posenet.draw_skel_and_kp(
+                    draw_image, pose_scores, keypoint_scores, keypoint_coords,
+                    min_pose_score=0.25, min_part_score=0)
+                cv2.imshow("out", draw_image)
+                cv2.waitKey(1)
+                cv2.imwrite(os.path.join(args.output_dir, os.path.relpath(f, args.image_dir)), draw_image)
+
+            if not args.notxt:
+                print()
+                print("Results for image: %s" % f)
+                for pi in range(len(pose_scores)):
+                    if pose_scores[pi] == 0.:
+                        break
+                    print('Pose #%d, score = %f' % (pi, pose_scores[pi]))
+                    for ki, (s, c) in enumerate(zip(keypoint_scores[pi, :], keypoint_coords[pi, :, :])):
+                        print('Keypoint %s, score = %f, coord = %s' % (posenet.PART_NAMES[ki], s, c))
+
+        print('Average FPS:', len(filenames) / (time.time() - start))
+
+
+if __name__ == "__main__":
+    main()
