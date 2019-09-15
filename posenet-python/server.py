99c8941efaf9584fd715fbaefe47eb90fbecfff4
Catherine Zeng <yczeng@mit.edu>
2019-09-15 07:27:37 -0400

    convert tabs to spaces

--- posenet-python/server.py
+++ posenet-python/server.py
@@ -22,18 +22,18 @@ args = parser.parse_args()
 
 # Called for every client connecting (after handshake)
 def new_client(client, server):
-	print("New client connected and was given id %d" % client['id'])
-	#server.send_message_to_all("Hey all, a new client has joined us")
+        print("New client connected and was given id %d" % client['id'])
+        #server.send_message_to_all("Hey all, a new client has joined us")
 
 
 # Called for every client disconnecting
 def client_left(client, server):
-	print("Client(%d) disconnected" % client['id'])
+        print("Client(%d) disconnected" % client['id'])
 
 
 # Called when a client sends a message
 def message_received(client, server, message):
-	print("Client(%d) said: %s" % (client['id'], message))
+        print("Client(%d) said: %s" % (client['id'], message))
         png_recovered = base64.decodebytes(message)
         img = np.frombuffer(png_recovered, dtype=np.float64)
 
@@ -51,7 +51,7 @@ def message_received(client, server, message):
         keypoint_coords *= output_scale
         result = keypoint_coords
         for pi in range(len(pose_scores)):
-	        if pose_scores[pi] == 0.:
+                if pose_scores[pi] == 0.:
                 result = keypoint_coords[:pi]
         
         server.send_message_to_all(result)
